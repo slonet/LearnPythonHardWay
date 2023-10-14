2c0f906eeef2ca4d655d9664acc4297abfae6a72
slonet <tyler.slone47@gmail.com>
2023-10-14 13:30:24 -0700

    added notes to ex 15

---
+++ ex15/ex15.py
@@ -0,0 +1,18 @@
+from sys import argv
+
+# read in command line argument for file name
+script, filename = argv
+
+# read the contents of the file into the txt variable
+txt = open(filename)
+
+print(f"Here's your file {filename}")
+print(txt.read())
+
+print("Type the filename again:")
+# get the same file name information from a terminal prompt instead of an argument
+file_again = input("> ")
+
+txt_again = open(file_again)
+
+print(txt_again.read())
