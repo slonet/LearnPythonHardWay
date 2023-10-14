fecbd58131b267f85dfcd1d91dddfc8d02a459bf
slonet <tyler.slone47@gmail.com>
2023-10-14 12:30:18 -0700

    added notes to ex 13

--- ex13/ex13.py
+++ ex13/ex13.py
@@ -6,3 +6,6 @@ print("The script is called:", script)
 print("Your first variable is:", first)
 print("Your second variable is:", second)
 print("Your third variable is", third)
+
+# 1st variable passed from argv is the name of the python file running
+# subsequent variables are collected from the terminal arguments entered when the script was run
