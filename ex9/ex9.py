bd2a17c827490dffb26c549157c4524548bbb4af
slonet <tyler.slone47@gmail.com>
2023-10-13 19:37:49 -0700

    completed exercise 8

---
+++ ex9/ex9.py
@@ -0,0 +1,14 @@
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+print("""
+This is a block string
+you can make a comment on multiple lines
+you don't need to comment out each line individually
+	tabs and new lines are included in the string.
+""")
