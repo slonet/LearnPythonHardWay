22c4ea269cd392373c48fc4b5ba7a8c83e41370f
slonet <tyler.slone47@gmail.com>
2023-10-14 14:47:45 -0700

    completed ex16

---
+++ ex16/ex16.py
@@ -0,0 +1,43 @@
+""" file object methods
+close	 		Closes the file
+read 			Creates a file wrapper object for the specified file
+readline 		Reads one line of the file
+truncate 		Empties the file
+write('stuff')	Writes 'stuff' into the file
+seek(0)			Moves the read/write locaton to the beginning of the file
+"""
+
+from sys import argv
+
+script, filename = argv
+
+print(f"We're going to erase {filename}.")
+print("If you don't want that, hit CTRL-C (^C).")
+print("If you do want that, hit RETURN.")
+
+input("?")
+
+print("Opening the file...")
+target = open(filename, 'w')
+
+print("Truncating the file. Goodbye!")
+target.truncate()
+
+print("Now I'm going to ask you for three lines.")
+
+line1 = input("line 1: ")
+line2 = input("line 2: ")
+line3 = input("line 3: ")
+
+print("I'm going to write these to the file.")
+
+target.write(f"{line1}\n{line2}\n{line3}\n")
+
+print("and finally, we close it.")
+target.close()
+
+print("Do you want to read the file back? Hit RETURN to read the file.")
+input('?')
+
+target = open(filename)
+print(target.read())
