d61cf0de88bf70f7f1f6cf9fb0c6b2a9c7c3a620
slonet <tyler.slone47@gmail.com>
2023-10-16 16:08:03 -0700

    completed ex23

---
+++ ex22/ex22.py
@@ -0,0 +1,32 @@
+import sys
+script, input_encoding, error = sys.argv
+
+
+def main(language_file, encoding, errors):
+	line = language_file.readline()
+
+	if line:
+		print_line(line, encoding, errors)
+		return main(language_file, encoding, errors)
+
+
+def print_line(line, encoding, errors):
+	next_lang = line.strip()
+	raw_bytes = next_lang.encode(encoding, errors=errors)
+	cooked_string = raw_bytes.decode(encoding, errors=errors)
+
+	print(raw_bytes, "<===>", cooked_string)
+
+
+languages = open("languages.txt", encoding="utf-8")
+
+main(languages, input_encoding, error)
+
+
+""" Things I've never seen before
+- function inside a return statement (recursion?)
+- string.strip()
+- encode()
+- decode()
+- using open() with a specific encoding scheme
+"""
